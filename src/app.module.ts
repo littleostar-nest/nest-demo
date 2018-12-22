166cd4b16a347382463e8f9b534ec54ed7117663
littleOStar <qqduxingzhe@163.com>
2018-12-22 19:53:13 +0800

    the code tip is none!!!!!!!!; add typeorm; maybe later, learn nestjs;

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './controllers/app.controller';
-import { AppService } from './services/app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app/app.controller';
+import { AppService } from './app/app.service';
+import { Connection } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(),
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {
+  constructor(
+    private readonly connection: Connection,
+  ) {
+  }
 }
---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from '@nestjs/common';
+import { CatsService } from './cats.service';
+
+@Controller('cats')
+export class CatsController {
+
+  constructor(
+    private catsService: CatsService,
+  ) {
+  }
+
+  @Get()
+  findAll() {
+    return this.catsService.findAll();
+  }
+}
